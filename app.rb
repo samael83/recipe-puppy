fd6d0ad178a5bf0b181cebbc0e1aa375fc368ef7
Samael <samael803@gmail.com>
2019-03-17 10:11:48 +0200

    add initURL function

--- app.rb
+++ app.rb
@@ -2,9 +2,6 @@ require 'httparty'
 require 'json'
 require './libs/NormalizeInput.rb'
 
-# Globals
-base_url = 'http://www.recipepuppy.com/api/?i='
-
 # Recieve user ingredients input
 print "\n"
 puts '*' * 23
@@ -14,12 +11,19 @@ puts "Please specify one or more ingredients and we will match the best recipes 
 puts "Specify your ingredient here:"
 print "> "
 
+# Globals
+base_url = 'http://www.recipepuppy.com/api/?i='
 user_input = $stdin.gets.chomp
 
 # Normalize input 
 user_ingredients = NormalizeInput.to_array(user_input)
 
 # Init base URL for HTTP request
+def initURL(url, arr)
+    str = url + arr.join(',')
+    return str
+end
+final_url = initURL(base_url, user_ingredients)
 
 # Send HTTP GET request
     # if errors => handle errors
